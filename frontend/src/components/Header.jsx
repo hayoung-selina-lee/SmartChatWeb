9521ecb0c76c03005e4d407e38a5b8bc4037f537
leehy0321 <sgn0321@naver.com>
2024-06-23 22:05:35 -0700

    [Frontend] changed directory name (component -> components)

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../resources/SmartChatLogo.svg";
+import "../App.css"; // CSS import
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const toMainPage = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="header-title-logo">
+          <img src={logo} className="header-icon" alt="Smart Chat App logo" onClick={toMainPage} />
+          <h1 className="header-title">Smart Chat App</h1>
+        </div>
+        <nav className="nav">
+          <a href="#features">Features</a>
+          <a href="#plans">Plans</a>
+          <a href="#about">About Us</a>
+        </nav>
+      </div>
+      <div className="header-right">
+        <button className="sign-in">Sign In</button>
+        <button className="sign-up">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
